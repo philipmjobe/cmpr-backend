1992a80fcce4aa4099785f771506e9294eec5a73
Philip <philipmjobe@gmail.com>
2023-03-07 15:24:10 -0500

    adds create method in user_controller and tests creation of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email]
+    )
+    if user.save
+      render json: {message: "User created successfully"}, status: :created
+    else  
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
 end
