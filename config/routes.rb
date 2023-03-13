aba36d379a20e54cb34126d8e7bfcb58b33522d2
Philip <philipmjobe@gmail.com>
2023-03-13 15:23:41 -0400

    creats routes for the campgrounds in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#destroy"
+
+  ### CAMPGROUNDS ROUTES ###
+  get "/campgrounds" => "campgrounds#index"
+  post "/campgrounds" => "campgrounds#create"
+  get "/campgrounds/:id" => "campgrounds#show"
+  patch "/campgrounds/:id" => "campgrounds#update"
+  delete "/campgrounds/:id" => "campgrounds#destroy"
 end
