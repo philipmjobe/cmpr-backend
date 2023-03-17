c31fd3c7573e89308af051c668b2af95d287ae2e
Philip <philipmjobe@gmail.com>
2023-03-17 12:52:33 -0400

    recreates user and create method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_164716) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_165104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_164716) do
     t.string "amenities"
     t.string "state"
     t.string "nearest_town"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
