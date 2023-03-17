da697115e61763474caa8ec03a2356cdc3a84a25
Philip <philipmjobe@gmail.com>
2023-03-17 12:50:06 -0400

    removes users table model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_190539) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_164716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,14 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_190539) do
     t.string "amenities"
     t.string "state"
     t.string "nearest_town"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
